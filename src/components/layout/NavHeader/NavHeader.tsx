9136375f72c5fac1e68dcbea862b4fd62cbea6e2
Emmanuel <emmsom506@gmail.com>
2024-07-30 13:51:28 +0000

    shifted the menu items little left

--- src/components/layout/NavHeader/NavHeader.tsx
+++ src/components/layout/NavHeader/NavHeader.tsx
@@ -24,40 +24,43 @@ const NavHeader = () => {
         <>
             <nav className="fixed top-0 w-full text-center bg-white px-4 py-2">
                 <div className="flex items-center justify-between">
-                    <div className="flex items-center">
-                        <KjcImage
-                            src="path/to/your/image.jpg" // Provide the correct path to the image
-                            alt="KcjImage"
-                            className="w-24"
-                            onClick={handleOnLogoClick}
-                        />
-                    </div>
-                    <div className="mx-auto text-center hidden md:block">
-                        <div className="flex items-center justify-evenly space-x-10 lg:space-x-12">
-                            {headerMenuOptions.map((menuItem) => {
-                                const isActive = activeItem === menuItem.key;
-                                return (
-                                    <div
-                                        key={menuItem.key}
-                                        className={`relative flex items-center text-2xl text-kjc-950 font-semibold cursor-pointer hover:text-kjcBtn-900 active:text-kjcBtn-900 group ${isActive ? 'text-kjcBtn-900' : ''}`}
-                                        onClick={() => {
-                                            setActiveItem(menuItem.key || "");
-                                            if (menuItem.onClick) {
-                                                menuItem.onClick();
-                                            }
-                                        }}
-                                    >
-                                        {menuItem.key}
-                                        <span
-                                            className={`absolute -bottom-6 left-0 w-full h-1 bg-kjcBtn-900 scale-x-0 group-hover:scale-x-100 transition-transform origin-right ${isActive ? 'scale-x-100' : ''}`}
-                                        ></span>
-                                    </div>
-                                );
-                            })}
+                    <div className="flex items-center space-x-24">
+                        <div className={"flex me-12"}>
+                            <KjcImage
+                                src="path/to/your/image.jpg" // Provide the correct path to the image
+                                alt="KcjImage"
+                                className="w-24"
+                                onClick={handleOnLogoClick}
+                            />
+                        </div>
+                        <div className="ps-16 items-center justify-between hidden md:block">
+                            <div className="flex items-center justify-evenly space-x-10 lg:space-x-12">
+                                {headerMenuOptions.map((menuItem) => {
+                                    const isActive = activeItem === menuItem.key;
+                                    return (
+                                        <div
+                                            key={menuItem.key}
+                                            className={`relative flex items-center text-xl font-semibold cursor-pointer hover:text-kjcBtn-900 active:text-kjcBtn-900 group ${isActive ? 'text-kjcBtn-900' : ''}`}
+                                            onClick={() => {
+                                                setActiveItem(menuItem.key || "");
+                                                if (menuItem.onClick) {
+                                                    menuItem.onClick();
+                                                }
+                                            }}
+                                        >
+                                            {menuItem.key}
+                                            <span
+                                                className={`absolute -bottom-8 left-0 w-full h-1 bg-kjcBtn-900 scale-x-0 group-hover:scale-x-100 transition-transform origin-right ${isActive ? 'scale-x-100' : ''}`}
+                                            ></span>
+                                        </div>
+                                    );
+                                })}
+                            </div>
                         </div>
                     </div>
+
                     <div className="flex items-center justify-end">
-                        <NavHeaderAuthBtn />
+                        <NavHeaderAuthBtn/>
                         {/*<LeftHeaderIcons activeItem={activeItem} setActiveItem={setActiveItem} />*/}
                     </div>
                 </div>
