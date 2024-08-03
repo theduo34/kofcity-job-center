fbbeddfaaa0a13bdf84e0f2ff0bc48cb7bd10419
Emmanuel <emmsom506@gmail.com>
2024-08-03 20:48:26 +0000

    Resolves #18

---
+++ src/components/user/Dashboard/components/SuccessStories/SuccessStories.constants.tsx
@@ -0,0 +1,63 @@
+export interface SuccessStoriesProps {
+    quote: string;
+    author: string;
+    jobTitle: string;
+}
+
+export const getSuccessStories = (): SuccessStoriesProps[] => [
+    {
+        quote: "Hey there. I'm doing great in coding. How about you? Thanks to Kofcity Job Center for the support!",
+        author: "aquasiNinetyNine",
+        jobTitle: "Software Engineer",
+    },
+    {
+        quote: "This platform has helped me achieve my dreams. Kofcity Job Center is amazing!",
+        author: "johnDoe",
+        jobTitle: "Product Manager",
+    },
+    {
+        quote: "An incredible journey with amazing opportunities. Kofcity Job Center made it possible.",
+        author: "janeDoe",
+        jobTitle: "UX Designer",
+    },
+    {
+        quote: "I couldn't have done it without this amazing community. Kofcity Job Center is the best!",
+        author: "mikeSmith",
+        jobTitle: "Backend Developer",
+    },
+    {
+        quote: "The support here is unmatched! Thank you, Kofcity Job Center!",
+        author: "saraLee",
+        jobTitle: "Frontend Developer",
+    },
+    {
+        quote: "Finding a job in the banking sector was made so easy by Kofcity Job Center.",
+        author: "maryJohnson",
+        jobTitle: "Bank Teller",
+    },
+    {
+        quote: "Thanks to Kofcity Job Center, I found my dream job in hospitality.",
+        author: "jamesBrown",
+        jobTitle: "Hotel Manager",
+    },
+    {
+        quote: "Transitioning to a new career in engineering was seamless with Kofcity Job Center.",
+        author: "robertWilliams",
+        jobTitle: "Mechanical Engineer",
+    },
+    {
+        quote: "I never thought I'd find a job I love as a storekeeper, but Kofcity Job Center made it happen.",
+        author: "lindaDavis",
+        jobTitle: "Storekeeper",
+    },
+    {
+        quote: "Becoming a professional barber was a dream come true, thanks to Kofcity Job Center.",
+        author: "michaelMiller",
+        jobTitle: "Barber",
+    },
+    {
+        quote: "Working in catering has been a fantastic experience, all thanks to Kofcity Job Center.",
+        author: "elizabethGarcia",
+        jobTitle: "Catering Manager",
+    },
+];
--- src/components/user/Dashboard/components/SuccessStories/SuccessStories.tsx
+++ src/components/user/Dashboard/components/SuccessStories/SuccessStories.tsx
@@ -1,50 +1,71 @@
+import React, { useState } from 'react';
 import WithContentLayout from "../../../../layout/hoc/WithContentLayout";
-import KjcImage from "../../../../../builders/KjcImage";
-import successTwo from "/public/assets/images/user/dashboard/successTwo.gif";
-import {UserOutlined} from "@ant-design/icons";
+import { UserOutlined } from "@ant-design/icons";
+import {getSuccessStories} from "./SuccessStories.constants.tsx";
+
 
 const SuccessStories = () => {
-    return(
+    const storiesPerPage = 2;
+    const [currentPage, setCurrentPage] = useState(1);
+    const stories = getSuccessStories();
+
+    const indexOfLastStory = currentPage * storiesPerPage;
+    const indexOfFirstStory = indexOfLastStory - storiesPerPage;
+    const currentStories = stories.slice(indexOfFirstStory, indexOfLastStory);
+
+    const totalPages = Math.ceil(stories.length / storiesPerPage);
+
+    const handlePageChange = (page: number) => {
+        setCurrentPage(page);
+    };
+
+    return (
         <>
-            <div className={"items-center justify-center  w-full"}>
-                <div className={"p-4 text-balance relative text-center text-xl md:text-2xl py-10 font-mono text-kjc-950 bg-amber-50 bg-opacity-80"}>
+            <div className="items-center justify-center w-full">
+                <div className="px-4 text-balance relative text-center text-xl md:text-2xl py-10 font-mono text-kjc-950 bg-amber-50 bg-opacity-80">
                     Real Stories, Real Success!!! Achieve Your Dreams with Us
                 </div>
-                <div className={"absolute top-1 right-4"}></div>
+                <div className="absolute top-1 right-4"></div>
 
-                <div className={"item-center"}>
-                    <WithContentLayout className={"items-center "}>
-                        <div className={"flex flex-col lg:flex-row items-center justify-start lg:justify-between xl:justify-around  space-y-14"}>
-                            <div
-                                className={"relative bg-white flex items-center justify-center h-[300px] w-[300px] xl:w-[400px] xl:h-[400px] shadow-lg"}>
+                <div className="items-center">
+                    <WithContentLayout className="items-center">
+                        <div className="flex flex-col lg:flex-row items-center justify-start lg:justify-between xl:justify-around space-y-14">
+                            {currentStories.map((story, index) => (
                                 <div
-                                    className={"absolute -bottom-10 left-10 flex items-center justify-center h-[300px] w-[300px] xl:w-[400px] xl:h-[400px] bg-amber-100"}>
-                                    <KjcImage
-                                        src={successTwo}
-                                        alt={"success gif"}
-                                        className={"z-10"}
-                                    />
-                                </div>
-                            </div>
-
-                            <div
-                                className={"relative bg-amber-100 flex items-center justify-center h-[300px] w-[300px] xl:w-[400px] xl:h-[400px]  shadow-lg "}>
-                                <div
-                                    className={"p-8 absolute -bottom-10 left-10 flex items-center justify-center h-[300px] w-[300px] xl:w-[400px] xl:h-[400px] bg-white"}>
-                                    <h1 className={"absolute text-4xl top-10  left-8 items-center justify-start font-semibold"}>"</h1>
-                                    <p>Hey there. I'm doing fucking good in coding. What the fuck you doing as well</p>
-                                    <h1 className={"absolute text-4xl bottom-10  right-8 items-center justify-start font-semibold"}>"</h1>
-                                    <div className={"absolute bottom-4 right-4"}>
-                                        <h1 ><UserOutlined/> aquasiNinetyNine</h1>
-                                        <p>Software Engineer</p>
+                                    key={index}
+                                    className="relative bg-kjcBtn-950 opacity-80 flex items-center justify-center h-[300px] w-[300px] xl:w-[400px] xl:h-[400px] shadow-lg"
+                                >
+                                    <div className="p-8 absolute border-r-2 border-r-kjcBtn-950 -bottom-10 left-10 flex items-center justify-center h-[300px] w-[300px] xl:w-[400px] xl:h-[400px] bg-white">
+                                        <h1 className="absolute text-4xl text-balance top-10 left-8 items-center justify-start font-semibold">"</h1>
+                                        <p>
+                                            {story.quote}
+                                        </p>
+                                        <h1 className="absolute text-4xl bottom-10 right-8 items-center justify-start font-semibold">"</h1>
+                                        <div className="absolute bottom-4 right-4">
+                                            <h1 className="flex items-center">
+                                                <UserOutlined /> {story.author}
+                                            </h1>
+                                            <p>{story.jobTitle}</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                            ))}
+                        </div>
+                        <div className="mt-12 flex justify-center">
+                            {Array.from({ length: totalPages }, (_, index) => (
+                                <button
+                                    key={index}
+                                    onClick={() => handlePageChange(index + 1)}
+                                    className={`px-4 py-2 mx-1 border-0 rounded-lg ${currentPage === index + 1 ? 'bg-kjcBtn-300 text-white' : 'bg-white hover:bg-gray-200 '}`}
+                                >
+                                    {index + 1}
+                                </button>
+                            ))}
                         </div>
                     </WithContentLayout>
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
 export default SuccessStories;
