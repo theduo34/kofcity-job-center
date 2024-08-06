a8cee74f1eeec1ca3c72b30519eff5f08a9e4ac2
Emmanuel <emmsom506@gmail.com>
2024-08-06 12:23:31 +0000

    Resolves #25

--- src/components/user/JobListings/JobListings.tsx
+++ src/components/user/JobListings/JobListings.tsx
@@ -6,7 +6,7 @@ const JobListings = () => {
     return(
         <>
             <div>
-                <div className={"items-center py-4 md:py-8 lg:py-12 px-0 md:px-8 lg:px-12 text-lg md:text-xl lg:text-3xl shadow-md font-semibold text-black bg-kjcBtn-200 "}>
+                <div className={"items-center py-4 md:py-8 px-0 md:px-8 lg:px-12 text-lg md:text-xl lg:text-3xl shadow-md font-semibold text-black bg-kjcBtn-200 "}>
                    <h1 className={"px-4 md:px-12 mx-0 md:ms-4 lg:ms-6 xl:ms-8"}> Search results for
                        <span className={"text-white mx-3"}>Most popular jobs in Koforidua</span>
                    </h1>
--- src/components/user/JobListings/components/FilterSearch/components/SearchModal.tsx
+++ src/components/user/JobListings/components/FilterSearch/components/SearchModal.tsx
@@ -1,5 +1,8 @@
 import KjcModal from "../../../../../../builders/KjcModal";
-import {Checkbox, Form, FormProps} from "antd";
+import {Checkbox, Form, FormProps, Radio} from "antd";
+import {
+    getSearchInputSelects
+} from "../../../../Dashboard/components/JobRecommendation/JobRecommendation.constants.tsx";
 
 export interface SearchModalProps {
     title?: string;
@@ -18,6 +21,8 @@ type FieldType = {
 
 const SearchModal = (props: SearchModalProps) => {
     const [form] = Form.useForm();
+
+    const filterInputSelects = getSearchInputSelects();
     const onFinish: FormProps<FieldType>["onFinish"] = (value) => {
         console.log(value)
     }
@@ -25,6 +30,7 @@ const SearchModal = (props: SearchModalProps) => {
     const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (value) => {
         console.log(value)
     }
+
     return (
         <>
             <KjcModal
@@ -35,9 +41,10 @@ const SearchModal = (props: SearchModalProps) => {
                 closable
                 footer={null}
                 width={"100%"}
-                style={{ width: '100%', height: '100%' }}
+                style={{ width: '100%', minHeight: '100%' }}
+                className={"bg-white"}
             >
-                <div className="relative h-screen items-center w-full">
+                <div className="relative h-screen items-center w-full min-h-screen bg-white">
                     {/* Top nav section */}
                     <div className="fixed top-0 left-0 right-0 py-4 text-xl md:text-2xl font-semibold items-center px-4 md:px-8 border-b-2 border-gray-200 bg-white z-10">
                         Filters
@@ -49,36 +56,41 @@ const SearchModal = (props: SearchModalProps) => {
                         name="basic"
                         onFinish={onFinish}
                         onFinishFailed={onFinishFailed}
+                        className={"min-h-screen bg-white w-full"}
                     >
-                        <div className="pt-16 pb-16 px-4 md:px-8">
+                        <div className="pt-16 pb-16 px-4 md:px-8 w-full bg-white">
                             <Form.Item<FieldType>
                                 name={"remote_only"}
                                 className={"items-center justify-start"}
                             >
-                                <Checkbox className={"text-xl font-normal items-center leading-tight px-2"}>Remote Jobs only</Checkbox>
+                                <Checkbox className={"text-xl font-semibold items-center leading-tight px-2"}>Remote Jobs only</Checkbox>
                             </Form.Item>
-                            <div
-                                className="flex flex-col md:flex-row items-start w-full justify-start md:justify-around">
-                                <div>Industry</div>
-                                <div>Job Type</div>
-                                <div>Location</div>
-                                <div>Role Level</div>
+                            <div className="flex flex-col md:flex-row items-start w-full justify-start md:justify-around">
+                                {
+                                    filterInputSelects.map((filterInput) => (
+                                        <div className="items-center text-lg font-normal leading-normal">
+                                            <h3 className="pt-4 font-semibold ps-2">{filterInput.name}</h3>
+                                            <Radio.Group>
+                                                {filterInput.options?.map((option) => (
+                                                    <p className="text-2xl my-4">
+                                                        <Radio value={option.value}>
+                                                            {option.label}
+                                                        </Radio>
+                                                    </p>
+                                                ))}
+                                            </Radio.Group>
+                                        </div>
+                                    ))
+                                }
                             </div>
                         </div>
 
                         {/* Footer buttons */}
-                        <div
-                            className="fixed bottom-0 left-0 right-0 flex justify-end items-center space-x-8 p-4 bg-white border-t-2 border-gray-200 z-10">
-                            <p
-                                className="font-semibold underline hover:text-kjcBtn-300 cursor-pointer"
-                                onClick={props.onCancel}
-                            >
+                        <div className="fixed bottom-0 left-0 right-0 flex justify-end items-center space-x-8 p-4 bg-white border-t-2 border-gray-200 z-10">
+                            <p className="font-semibold underline hover:text-kjcBtn-300 cursor-pointer" onClick={props.onCancel}>
                                 Cancel
                             </p>
-                            <p
-                                className="py-3 px-6 bg-kjcBtn-200 cursor-pointer rounded-lg hover:bg-kjcBtn-300 font-semibold"
-                                onClick={props.onOk}
-                            >
+                            <p className="py-3 px-6 bg-kjcBtn-200 cursor-pointer rounded-lg hover:bg-kjcBtn-300 font-semibold" onClick={props.onOk}>
                                 View Result
                             </p>
                         </div>
