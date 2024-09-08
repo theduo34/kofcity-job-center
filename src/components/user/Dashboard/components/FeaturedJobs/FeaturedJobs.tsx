8cdca96a9dee9e68e89c21345c9f9e98bdfb73ba
Emmanuel <emmsom506@gmail.com>
2024-09-08 17:22:37 +0000

    fix job recommedation card responsive issue

--- src/components/user/Dashboard/components/FeaturedJobs/FeaturedJobs.tsx
+++ src/components/user/Dashboard/components/FeaturedJobs/FeaturedJobs.tsx
@@ -1,78 +1,65 @@
 import KjcCarousel from "../../../../../builders/KjcCarousel";
 import KjcButton from "../../../../../builders/KjcButton";
-import {getFeaturedJobs} from "./FeaturedJobs.constants.tsx";
+import { getFeaturedJobs } from "./FeaturedJobs.constants.tsx";
 import { Bookmark } from 'lucide-react';
+import { useState } from "react";
 
+//eslint-disable-next-line
+const JobDetail = ({ label, value }: { label: string; value: any }) => (
+    <p className="items-center">
+        <span className="text-lg md:text-2xl font-mono capitalize">{label}: </span>
+        <span className="items-center text-lg md:text-xl font-normal">{value}</span>
+    </p>
+);
 
-/**
- * Renders the featured jobs component.
- *
- * @return {JSX.Element} The rendered featured jobs component.
- * @return {featured jobs} high demand jobs with competitive salary
- */
 const FeaturedJobs = () => {
+    const [bookmarkedJob, setBookmarkedJob] = useState<{ [key: string]: boolean }>({});
     const featuredJobs = getFeaturedJobs();
+
+    const toggleBookmark = (key: string) => {
+        setBookmarkedJob((prevState) => ({
+            ...prevState,
+            [key]: !prevState[key],
+        }));
+    };
+
     return (
-        <div className="items-center ">
-            <div className="items-center leading-tight font-mono text-xl md:text-3xl pb-8 text-kjc-950">
-                Featured Jobs
-            </div>
-            <div className="relative py-4 items-center border-2 border-neutral-200 rounded-lg">
+        <div className="items-center">
+            <h2 className="leading-tight font-mono text-xl md:text-3xl pb-8 text-kjc-950">Featured Jobs</h2>
+            <div className="relative py-4 border-2 border-neutral-200 rounded-lg">
                 <KjcCarousel
                     arrows
                     dots={true}
                     dotsPosition="bottom"
-                    className="kjc-carousel items-center rounded-lg"
+                    className="kjc-carousel rounded-lg"
+                    autoplay={true}
+                    infinite={false}
+                    autoplaySpeed={2000}
                 >
-                    {
-                        featuredJobs.map( featuredJob => (
-                            <div
-                                className="items-center px-8 lg:px-16 pb-14 ">
-                                <div className="items-center space-y-2 py-2 md:py-8 md:space-y-4">
-                                    <p className={"items-center"}>
-                                        <span className={"text-xl md:text-3xl text-kjc-950  font-mono capitalize"}>  </span>
-                                        <span
-                                            className={"text-xl md:text-3xl items-center font-mono "}> {featuredJob.jobTitle}</span>
-                                    </p>
-                                    <p className={"items-center"}>
-                                        <span
-                                            className={" text-lg md:text-2xl font-mono capitalize"}> Company Name: </span>
-                                        <span className={"items-center  text-lg md:text-xl font-normal"}> {featuredJob.companyName}</span>
-                                    </p>
-                                    <p className={"items-center"}>
-                                        <span className={"text-lg md:text-2xl font-mono capitalize"}> Skills: </span>
-                                        <span className={"items-center text-lg md:text-xl font-normal"}> {featuredJob.jobSkills}</span>
-                                    </p>
-                                    <p className={"items-center"}>
-                                        <span className={"text-lg md:text-2xl font-mono capitalize"}> Job Type: </span>
-                                        <span className={"items-center text-lg md:text-xl font-normal"}> {featuredJob.jobType}</span>
-                                    </p>
-                                    <p className={"items-center"}>
-                                        <span className={"text-lg md:text-2xl font-mono capitalize"}> Location: </span>
-                                        <span className={"items-center text-lg md:text-xl font-normal"}> {featuredJob.jobLocation}</span>
-                                    </p>
-                                    <p className={"items-center"}>
-                                        <span className={"text-lg md:text-2xl font-mono capitalize"}>Education: </span>
-                                        <span className={"items-center text-lg md:text-xl font-normal"}> {featuredJob.education}</span>
-                                    </p>
-                                </div>
-                                <div className={"w-full pt-8 flex xl:w-2/5 items-center justify-between"}>
-                                    <div>
-                                        <KjcButton
-                                            className={" py-6 px-12 rounded-lg border-2 border-kjcBtn-300 text-lg hover:cursor-pointer hover:-translate-y-1 hover:scale-110  duration-300 "}
-                                        >
-                                            Apply Now
-                                        </KjcButton>
-                                    </div>
-                                    <div className={"flex absolute top-0 right-2 md:right-8 items-center space-x-4 text-2xl"}>
-                                        <div>
-                                            <Bookmark/>
-                                        </div>
-                                    </div>
-                                </div>
+                    {featuredJobs.map(({ jobTitle, companyName, jobSkills, jobType, jobLocation, education, key }) => (
+                        <div className="relative items-center px-8 lg:px-16 pb-14" key={key}>
+                            <div className="space-y-2 py-2 md:py-8 md:space-y-4">
+                                <h3 className="text-xl md:text-3xl font-mono capitalize">{jobTitle}</h3>
+                                <JobDetail label="Company Name" value={companyName} />
+                                <JobDetail label="Skills" value={jobSkills} />
+                                <JobDetail label="Job Type" value={jobType} />
+                                <JobDetail label="Location" value={jobLocation} />
+                                <JobDetail label="Education" value={education} />
                             </div>
-                        ))
-                    }
+                            <div className="w-full pt-8 flex xl:w-2/5 justify-between">
+                                <KjcButton
+                                    className="py-6 px-12 rounded-lg border-2 border-kjcBtn-300 text-lg hover:cursor-pointer hover:-translate-y-1 hover:scale-110 duration-300"
+                                >
+                                    Apply Now
+                                </KjcButton>
+                            </div>
+
+                            {/* Bookmark button */}
+                            <div className="absolute top-4 right-6 z-10 cursor-pointer" onClick={() => toggleBookmark(key as string)}>
+                                {bookmarkedJob[key as string] ? <Bookmark style={{ fill: "black" }} /> : <Bookmark />}
+                            </div>
+                        </div>
+                    ))}
                 </KjcCarousel>
             </div>
         </div>
